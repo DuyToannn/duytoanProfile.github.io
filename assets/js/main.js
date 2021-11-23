e2970d3eff8f9e9e2a1ef028733809a81fb5e58b
Duy Toan <toanpmo123@gmail.com>
2021-11-23 21:24:23 +0700

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,7 +46,8 @@ window.addEventListener('scroll', scrollActive)
 /* ============== change header */
 function scrollHeader(){
     const header = document.getElementById('header')
-    if(this.scrollY >= 200) header.classList.add('scroll-header');else header.classList.remove('scroll-header')
+    if(this.scrollY >= 200) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll',scrollHeader)
 
